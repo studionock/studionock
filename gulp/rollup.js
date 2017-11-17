a5644c44db87ac67f2d36ad2ee2c22e0f9fb5835
Adam Bergman <adam@fransvilhelm.com>
2017-11-17 15:50:57 +0100

    :wrench: Update rollup config

--- gulp/rollup.js
+++ gulp/rollup.js
@@ -1,24 +1,36 @@
 import { rollup as roll } from 'rollup';
 import babel from 'rollup-plugin-babel';
+import replace from 'rollup-plugin-replace';
+import uglify from 'rollup-plugin-uglify';
 import gutil from 'gulp-util';
 
+const isProd = process.env.NODE_ENV !== 'production';
+
 function task({ src, dest, name }) {
+  const plugins = [
+    babel({
+      babelrc: false,
+      presets: [['env', { modules: false }]],
+      plugins: ['external-helpers'],
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(
+        process.env.NODE_ENV === 'production' ? 'production' : 'development',
+      ),
+    }),
+    !isProd && uglify(),
+  ].filter(p => p !== null);
+
   const inputOptions = {
     input: src,
-    plugins: [
-      babel({
-        babelrc: false,
-        presets: [['env', { modules: false }]],
-        plugins: ['external-helpers'],
-      }),
-    ],
+    plugins,
   };
 
   const outputOptions = {
     file: dest,
     format: 'umd',
     name,
-    sourcemap: process.env.NODE_ENV !== 'production',
+    sourcemap: isProd,
   };
 
   return async function rollup() {
